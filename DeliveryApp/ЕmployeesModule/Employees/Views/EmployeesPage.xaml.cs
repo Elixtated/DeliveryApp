204e7e7c085cad4d2a70dd9a28b6ca6d43d2af5e
Viktor Borodinov <urmindisendless1642@gmail.ru>
2020-07-24 00:54:14 +0400

    Added new EmployeesModule

---
+++ DeliveryApp/ЕmployeesModule/Employees/Views/EmployeesPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Windows.Controls;
+
+namespace DeliveryApp.Orders.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для OrdersPage.xaml
+    /// </summary>
+    public partial class OrdersPage : Page
+    {
+        public OrdersPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
