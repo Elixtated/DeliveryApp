09b89aa13160cf86894676f4f8a47c13a9b664ae
Elixtated <urmindisendless1642@gmail.com>
2020-07-26 23:21:43 +0400

    First variant of Navigate Employees and Orders Menue

--- DeliveryApp/DeliveryApp/MainWindow/ViewModels/MainWindowViewModel.cs
+++ DeliveryApp/DeliveryApp/MainWindow/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using System.ComponentModel;
-
+using ЕmployeesModule.Employees.Views;
 using CommonModule.Services;
 using System.Windows.Input;
 using CommonModule.CommonTools;
 using DeliveryApp.Orders.Views;
+using System.Windows.Controls;
 
 namespace DeliveryApp.MainWindow.ViewModels
 {
@@ -11,20 +12,27 @@ namespace DeliveryApp.MainWindow.ViewModels
     {
         public MainWindowViewModel()
         {
+            OrdersPage = new OrdersPage();
+            EmployeesPage = new EmployeesPage();
             NavigatorService = NavigatorService.GetInstance();
-            NavigatorService.SetCurrentPage(new OrdersPage());
+            NavigatorService.SetCurrentPage(OrdersPage);
 
             OpenOrdersMenuCommand = new RelayCommand(OpenOrdersMenu);
+            OpenEmployeesMenuCommand = new RelayCommand(OpenEmployeesMenu);
+            
         }
-
+        
         public ICommand OpenOrdersMenuCommand { get; }
         public ICommand OpenEmployeesMenuCommand { get; }
+
+        public Page OrdersPage { get; set; }
+        public Page EmployeesPage { get; set; }
 
         public NavigatorService NavigatorService { get; private set; }
 
         private void OpenOrdersMenu()
         {
-            NavigatorService.SetCurrentPage(new OrdersPage());
+            NavigatorService.SetCurrentPage(OrdersPage);
         }
 
         private void OpenEmployeesMenu()
